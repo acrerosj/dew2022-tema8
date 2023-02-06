b32fc0ffa0101b302d70df906151cfe33ee9c9ec
Andrés Crespo <acrerosj@canariaseducacion.es>
2023-02-06 17:10:54 +0000

    intro a vue conceptos básicos

--- vue1.js
+++ vue1.js
@@ -6,6 +6,19 @@ createApp({
       message: 'Hola Vue!',
       saludo: '¡que pasa!',
       modulo: 'DSW',
+      mostrar: false,
+      frutas: ["manzana", "pera", "platano", "naranja", "kiwi"],
+      fruta: null,
+      cliente: {
+        id: 5,
+        name: 'Pepe',
+        age: 33,
+      }
     }
-  }
+  },
+  methods: {
+    mayusculas() {
+      this.cliente.name = this.cliente.name.toUpperCase();
+    }
+  },
 }).mount('#app')
